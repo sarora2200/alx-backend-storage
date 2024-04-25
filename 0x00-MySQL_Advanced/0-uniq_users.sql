28cad0028c4d4b5ede66a628f61263ceaa638fe7
sarora2200 <sarora2200@gmail.com>
2024-04-25 06:46:59 +0000

    done

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--Creates a table of users
+DROP TABLE IF EXITS users;
+CREATE TABLE users (
+	id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
