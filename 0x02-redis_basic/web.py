d63cd840c746a5ae000c0d0f312f1f3d1f75fc27
root <sarora2200@gmail.com>
2024-05-01 14:14:46 +0000

    done

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,15 +14,16 @@ def count_url_access(method):
     a URL is accessed """
     @wraps(method)
     def wrapper(url):
+        count_key = "count:" + url
+        store.incr(count_key)
+
         cached_key = "cached:" + url
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        count_key = "count:" + url
         html = method(url)
 
-        store.incr(count_key)
         store.set(cached_key, html)
         store.expire(cached_key, 10)
         return html
